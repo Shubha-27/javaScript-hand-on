4b84cf87b4c1bb9889ea79d47d724879d329d2a7
Shubha-27 <shubhangij784@gmail.com>
2025-07-29 09:44:06 +0530

    01-variable-assignment.js
    variable

---
+++ 01-variable-assignment.js
@@ -0,0 +1,20 @@
+console.log("=================Variable Assignment=================== ");
+
+var city="Dharashiv";
+console.log("My city name is: " +city);
+
+var bestFriend="Aai";
+console.log("My best friend name is: " +bestFriend);
+
+var country="India";
+console.log("My Country name is: " +country);
+
+var school="Pratibha Niketan Vidhyalaya Murum";
+console.log("My School name is: " +school);
+
+var college="Sangameshwar College Solapur";
+console.log("My College name is: "+college);
+
+
+
+
