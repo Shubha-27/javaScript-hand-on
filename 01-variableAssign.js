7a5ffbb43528c96cd47191a811d903129ff81d8e
Shubha-27 <shubhangij784@gmail.com>
2025-07-29 09:45:01 +0530

    01-variableAssign.js
    variable

---
+++ 01-variableAssign.js
@@ -0,0 +1,51 @@
+console.log("========================variable Assignment-2=========================");
+
+console.log("============================Task-1==============================");
+var myFullName;
+console.log("Before Initialization the value of variable is: "+myFullName);
+
+myFullName="Shubhangi Jadhav";
+console.log("After Initialization the value of variable is: "+myFullName);
+
+myFullName="Shubhangi Rajendra Jadhav";
+console.log("Upating the variable of value is: "+myFullName);
+
+
+
+console.log("============================Task-2==============================");
+var myLove="I Love Only Js";
+console.log("My Love name is: "+myLove);
+
+
+console.log("============================Task-3==============================");
+var firstName="Shubhangi";
+console.log("My Name is: "+firstName);
+
+var lastName="Jadhav";
+console.log("My Surname is: "+lastName);
+
+var age=21;
+console.log("My Age is: "+age);
+
+var address="A/p Murum ,Tq.Omerga ,Dist.Dharashiv";
+console.log("My Address is: "+address);
+
+var pinCode=413605;
+console.log("My Dist pincode is: "+pinCode);
+
+var gender="Female";
+console.log("My gender is: "+gender);
+
+var isMarried="Single";
+console.log("My Marital status is: "+isMarried);
+
+
+
+
+
+
+
+
+
+
+
