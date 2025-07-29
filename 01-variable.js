56b464479a7ec53596fbbe8b5267532202edfb21
Shubha-27 <shubhangij784@gmail.com>
2025-07-29 09:44:32 +0530

    01-variable.js
    variable

---
+++ 01-variable.js
@@ -0,0 +1,24 @@
+//variable Declaration
+// Variable Declaration
+var firstName;
+
+console.log(" firstName - Variable Declaration:" + firstName);
+
+firstName = "Elon"; //Variable Initialization
+console.log("firstName - Variable Initialization: " + firstName);
+
+firstName = "Stew"; // Variable update
+console.log("firstName - Variable Update: " + firstName);
+
+firstName = "Avadhut"; // Variable update
+console.log("firstName - Variable Update: " + firstName);
+
+firstName = "Somesh"; // Variable update
+console.log("firstName - Variable Update: " + firstName);
+
+console.log("========= variable Declaration and Initialization in the same line  =======");
+var city = "Pune";
+console.log("City is: " + city);
+
+var college="Sangameshwar College Solapur"
+console.log("My College Name is:" +college);
