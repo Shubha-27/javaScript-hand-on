a06f2c6b4ade41919415a70a48eb3fd13718b5d6
Shubha-27 <shubhangij784@gmail.com>
2025-07-31 10:39:50 +0530

    02-typeof.js
    typeof operator

---
+++ 02-typeOf.js
@@ -0,0 +1,25 @@
+console.log("==================Assignment of Variable,data type and typeof operator=====================");
+console.log("===========================================Task-1==========================================");
+var bankName;
+console.log(`Before initialization bank name is: ${bankName} || and it's data type is: ${typeof bankName} `);
+bankName="SBI Bank";
+console.log(`After initialization bank name is: ${bankName} || and it's data type is: ${typeof bankName} `);
+console.log("===========================================Task-2==========================================");
+var marks='90%';
+console.log(`The Given Marks is: ${marks} || and it's datatype is: ${typeof marks}`);
+console.log("===========================================Task-3==========================================");
+var isWorking;
+console.log(`Before initialization Working is: ${isWorking} || and it's data type is: ${typeof isWorking}`);
+isWorking=true;
+console.log(`After initialization working is: ${isWorking} || and it's data type is: ${typeof isWorking}`);
+console.log("===========================================Task-4==========================================");
+var totalCount="Hundred and Seven";
+console.log(`Total Count is: ${totalCount} || and it's data type is: ${typeof totalCount}`);
+totalCount=107;
+console.log(`Updated total count value is: ${totalCount} || and it's data type is: ${typeof totalCount}`);
+
+
+
+
+
+
