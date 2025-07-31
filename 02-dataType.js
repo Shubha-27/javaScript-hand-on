ec09f2be3aa5d5fb39e1beef9f7dba12e2dc15fe
Shubha-27 <shubhangij784@gmail.com>
2025-07-31 10:39:01 +0530

    02-dataType.js
    datatype

--- 02-dataType.js
+++ 02-dataType.js
@@ -10,3 +10,16 @@ console.log("Type of age: "+ ageTypeOf);
 var weight = 67.89;
 console.log("Weight: "+weight);
 console.log("Type of weight is: " + typeof weight);
+
+var v1=true;
+console.log(typeof(true));
+
+console.log("====== 2. Boolean Data Type ========");
+var isMarried = true;
+console.log(`Is Married: ${isMarried}  and it's data type is: ${typeof isMarried}`);
+
+console.log("======= String Template =======");
+
+var isBachelor = false;
+// ES 6 Feature --> String Template
+console.log(`Is Bachelor:  ${isBachelor} and it's datatype is: ${typeof isBachelor}`);
