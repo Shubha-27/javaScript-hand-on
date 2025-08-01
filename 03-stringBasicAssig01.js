440418b1306e8bdf8f7d19df96ba2a70ed0577b2
Shubha-27 <shubhangij784@gmail.com>
2025-08-01 10:39:13 +0530

    03-stringBasicAssig01.js
    string

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,13 @@
+console.log(`=====================================Assignment-1======================================`);
+function stringBasic(){
+var dremCompany="Google";
+console.log(`My dream company is: ${dremCompany}`);
+hobby1="Kabbadi";
+hobby2="Dance";
+hobby3="Watching TV";
+console.log(`My Hobbies: ${hobby1}, ${hobby2}, ${hobby3}`);
+var sum=hobby1.length+hobby2.length+hobby3.length;
+console.log(`The Sum of total number of characters is available in hobby1,hobby2,hobby3 is: ${sum}`);
+}
+stringBasic();
+
